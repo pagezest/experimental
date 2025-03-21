85d1f5d797558ddd5dd25706cdcc8472200eb78a
Harshil Jani <harshiljani2002@gmail.com>
2025-03-22 03:42:01 +0530

    Multiple benchmarking
    Signed-off-by: Harshil Jani <harshiljani2002@gmail.com>

--- test_wasm/README.md
+++ test_wasm/README.md
@@ -1,9 +1,4 @@
-To compile the rust source code
-
-```
-cargo run -- tinywasm
-cargo run -- wasmi
-```
+# Generating Wasm binary
 
 For generating the wasm binary from assembly script
 Edit the index file `wasm-generator/assembly/index.ts` and build the binary
@@ -16,14 +11,58 @@ npm run build
 
 You can find the debug.wasm file generated inside `wasm-generator/build/debug.wasm`.
 
-To run benchmarks on `tinyWASM`
+# Quick Steps to run benchmarking
 
+To compile the rust source code and run benchmarks based on
+- Wasm VM to use.
+- Number of times you'd want to initialize a wasm vm.
+
+Run any of the following commands.
 ```
-cargo run -- tinywasm
+cargo run -- tinywasm 10
+cargo run -- wasmi 10
+cargo run -- wamr 10
 ```
 
-To run benchmarks on `wasmi`
+# Wamr Setup
 
+Please install 
+## 1. CMAKE > 3.1
+
+MacOS
+```shell
+brew install cmake
 ```
-cargo run -- wasmi
+
+Linux
+```shell
+sudo pacman -S cmake
+```
+
+## 2. LLVM
+
+MacOS
+```shell
+brew install llvm
+export LLVM_DIR=$(brew --prefix llvm)/lib/cmake/llvm
+```
+
+Linux
+```shell
+sudo pacman -S llvm
+export LLVM_DIR=/usr/bin/llvm
+```
+
+Keep your rust version > 1.82
+
+```shell
+rustup override set 1.82.0
+```
+
+
+### Run the Benchmark
+
+Use the following command
+```rust
+cargo run -- wamr 10
 ```
--- test_wasm/src/main.rs
+++ test_wasm/src/main.rs
@@ -1,9 +1,18 @@
-use std::env;
+use std::{ env, thread };
 use eyre::Result;
-use tinywasm::{ Module, Store };
 
 use psutil::process::Process;
+use std::path::PathBuf;
+
+use tinywasm::{ Module, Store };
 use wasmi::{ Engine, Linker, Module as WasmiModule, Store as WasmiStore };
+use wamr_rust_sdk::{
+    runtime::Runtime,
+    module::Module as WamrModule,
+    instance::Instance,
+    function::Function,
+    value::WasmValue,
+};
 
 fn get_process_memory() -> u64 {
     let pid = std::process::id();
@@ -11,130 +20,102 @@ fn get_process_memory() -> u64 {
     process.memory_info().unwrap().rss() / 1024 // Convert bytes to KB
 }
 
+#[derive(Debug, Clone, Copy)]
+enum WasmVM {
+    TinyWASM,
+    Wasmi,
+    Wamr,
+}
+
+impl WasmVM {
+    fn from_str(vm: &str) -> Option<Self> {
+        match vm {
+            "tinywasm" => Some(Self::TinyWASM),
+            "wasmi" => Some(Self::Wasmi),
+            "wamr" => Some(Self::Wamr),
+            _ => None,
+        }
+    }
+}
+
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
+    if args.len() < 3 {
         eprintln!("Please add an argument to specify WASM VM.");
-        println!("Try : cargo run -- <tinywasm|wasmi>");
+        println!("Try : cargo run -- <tinywasm|wasmi> <num_instance>");
         return Ok(());
     }
-    let wasm_vm = &args[1];
-    match wasm_vm.as_str() {
-        "tinywasm" => {
-            println!("-----TinyWASM-----");
-            for _ in 1..10 {
+    let wasm_vm = match WasmVM::from_str(&args[1]) {
+        Some(vm) => vm,
+        None => {
+            eprintln!("Invalid WASM VM.");
+            return Ok(());
+        }
+    };
+
+    let num_instances: usize = args[2].parse().unwrap_or(1);
+
+    println!("Running {} Instances of {:?}", num_instances, wasm_vm);
+
+    let handles: Vec<_> = (0..num_instances)
+        .map(|_| {
+            thread::spawn(move || {
                 let mem_before = get_process_memory();
-                println!("Memory before Wasm: {} KB", mem_before);
-                call_tiny_wasm()?;
+                //println!("Memory Before {} kB", mem_before);
+
+                (
+                    match wasm_vm {
+                        WasmVM::TinyWASM => call_tiny_wasm(),
+                        WasmVM::Wasmi => call_wasmi_wasm(),
+                        WasmVM::Wamr => call_wamr_runtime(),
+                    }
+                ).unwrap();
+
                 let mem_after = get_process_memory();
-                println!("Memory after Wasm: {} KB", mem_after);
-                println!("Wasm Overhead: {} KB", mem_after.saturating_sub(mem_before));
-            }
-        }
-        "wasmi" => {
-            println!("-----WASMI-----");
-            for _ in 1..10 {
-                let mem_before = get_process_memory();
-                println!("Memory before Wasm: {} KB", mem_before);
-                call_wasmi_wasm()?;
-                let mem_after = get_process_memory();
-                println!("Memory after Wasm: {} KB", mem_after);
-                println!("Wasm Overhead: {} KB", mem_after.saturating_sub(mem_before));
-            }
-        }
-        "2-tinywasm" => {
-            println!("-----2 Tinywasm VM-----");
-            for _ in 1..10 {
-                let mem_before = get_process_memory();
-                println!("Memory before Wasm: {} KB", mem_before);
-                call_tiny_wasm()?;
-                let mem_after = get_process_memory();
-                println!("Memory after Wasm: {} KB", mem_after);
-                println!("Wasm Overhead: {} KB", mem_after.saturating_sub(mem_before));
-                call_sub_tinywasm()?;
-                let mem_after_sub = get_process_memory();
-                println!("Memory after Wasm-2: {} KB", mem_after_sub);
-                println!("Wasm Overhead-2: {} KB", mem_after_sub.saturating_sub(mem_after));
-            }
-        }
-        "2-wasmi" => {
-            println!("-----2 WASMI VM-----");
-            for _ in 1..10 {
-                let mem_before = get_process_memory();
-                println!("Memory before Wasm: {} KB", mem_before);
-                call_wasmi_wasm()?;
-                let mem_after = get_process_memory();
-                println!("Memory after Wasm: {} KB", mem_after);
-                println!("Wasm Overhead: {} KB", mem_after.saturating_sub(mem_before));
-                call_sub_wasmi()?;
-                let mem_after_sub = get_process_memory();
-                println!("Memory after Wasm-2: {} KB", mem_after_sub);
-                println!("Wasm Overhead-2: {} KB", mem_after_sub.saturating_sub(mem_after));
-            }
-        }
-        _ => {
-            eprintln!("Invalid WasmVM. Use \"tinywasm\" or \"wasmi\"");
-        }
+                //println!("Memory After {} kB", mem_after);
+                println!("Wasm Overhead {} kB", mem_after.saturating_sub(mem_before));
+            })
+        })
+        .collect();
+    for handle in handles {
+        handle.join().unwrap();
     }
     Ok(())
 }
 
 fn call_tiny_wasm() -> Result<()> {
     let wasm_add = std::fs::read("debug.wasm")?;
-
     let add_module = Module::parse_bytes(&wasm_add)?;
-
     let mut store = Store::default();
-
-    // Instantiate the `add` module.
     let add_instance = add_module.instantiate(&mut store, None)?;
-
-    // Call the `main` function, which uses the imported `add` function.
     let main = add_instance.exported_func::<(i32, i32), i32>(&store, "add")?;
     let res = main.call(&mut store, (10, 20))?; // 10 + 20 = 30
-    println!("Result: {}", res);
-
+    assert_eq!(res, 30);
     Ok(())
 }
 
 fn call_wasmi_wasm() -> Result<()> {
     let wasm_add = std::fs::read("debug.wasm")?;
-
     let engine = Engine::default();
     let module = WasmiModule::new(&engine, wasm_add)?;
-
     let mut store = WasmiStore::new(&engine, ());
-
     let linker = Linker::new(&engine);
-
     let add_instance = linker.instantiate(&mut store, &module)?.start(&mut store)?;
     let add_func = add_instance.get_typed_func::<(i32, i32), i32>(&mut store, "add")?;
     let res = add_func.call(&mut store, (10, 20))?;
-    println!("Results : {}", res);
-
+    assert_eq!(res, 30);
     Ok(())
 }
 
-fn call_sub_tinywasm() -> Result<()> {
-    let wasm_sub = std::fs::read("sub.wasm")?;
-    let add_module = Module::parse_bytes(&wasm_sub)?;
-    let mut store = Store::default();
-    let add_instance = add_module.instantiate(&mut store, None)?;
-    let main = add_instance.exported_func::<(i32, i32), i32>(&store, "sub")?;
-    let res = main.call(&mut store, (20, 10))?;
-    println!("Result: {}", res);
-    Ok(())
-}
-
-fn call_sub_wasmi() -> Result<()> {
-    let wasm_sub = std::fs::read("sub.wasm")?;
-    let engine = Engine::default();
-    let module = WasmiModule::new(&engine, wasm_sub)?;
-    let mut store = WasmiStore::new(&engine, ());
-    let linker = Linker::new(&engine);
-    let sub_instance = linker.instantiate(&mut store, &module)?.start(&mut store)?;
-    let sub_func = sub_instance.get_typed_func::<(i32, i32), i32>(&mut store, "sub")?;
-    let res = sub_func.call(&mut store, (20, 10))?;
-    println!("Results : {}", res);
+fn call_wamr_runtime() -> Result<()> {
+    let runtime = Runtime::new().map_err(|e| eyre::eyre!(e))?;
+    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    d.push("debug.wasm");
+    let module = WamrModule::from_file(&runtime, d.as_path()).map_err(|e| eyre::eyre!(e))?;
+    let instance = Instance::new(&runtime, &module, 1024 * 64).map_err(|e| eyre::eyre!(e))?;
+    let function = Function::find_export_func(&instance, "add").map_err(|e| eyre::eyre!(e))?;
+    let params: Vec<WasmValue> = vec![WasmValue::I32(10), WasmValue::I32(20)];
+    let result = function.call(&instance, &params).map_err(|e| eyre::eyre!(e))?;
+    assert_eq!(result[0], WasmValue::I32(30));
     Ok(())
 }
